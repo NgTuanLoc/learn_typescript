1da686b654797bf8822a05c76609d307db0b6a9a
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-12-01 16:14:13 +0700

    map project

---
+++ 9-map/src/CustomMap.ts
@@ -0,0 +1,12 @@
+export class CustomMap {
+   private googleMap: google.maps.Map;
+   constructor(divId: string) {
+      this.googleMap = new google.maps.Map(document.getElementById(divId), {
+         zoom: 1,
+         center: {
+            lat: 2,
+            lng: 5,
+         },
+      });
+   }
+}
---
+++ 9-map/src/index.ts
@@ -0,0 +1,18 @@
+// import { User } from './User';
+// import { Company } from './Company';
+
+// const user = new User();
+// const company = new Company();
+// console.log(user);
+// console.log(company);
+
+// new google.maps.Map(document.getElementById('map'), {
+//    zoom: 4,
+//    center: {
+//       lat: 0,
+//       lng: 0,
+//    },
+// });
+
+import {CustomMap} from './CustomMap'
+new CustomMap("map")
