318c9bb521c4c463afb1549d5e591702e6f014c5
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-12-04 08:56:01 +0700

    add linked list

--- 10-sort/src/index.ts
+++ 10-sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numberArray = new NumbersCollection([12, -2, 3, -12, 34, 234, -2]);
 const sort = new Sorter(numberArray);
@@ -13,5 +14,16 @@ console.log(sort.collection);
 
 const characterArray = new CharactersCollection('asdmnfbbAzh');
 const newSort = new Sorter(characterArray);
-newSort.sort()
+newSort.sort();
 console.log(newSort.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(234);
+linkedList.add(-23);
+linkedList.add(3);
+linkedList.add(-1);
+linkedList.add(5);
+const linkedListSort = new Sorter(linkedList)
+linkedListSort.sort()
+linkedList.print()
+
