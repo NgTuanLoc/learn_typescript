8bb7b0b71a077dabb3ba3feebeb2be44f792305f
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-12-21 08:50:21 +0700

    generics with function

--- 12-generics.ts/generics.ts
+++ 12-generics.ts/generics.ts
@@ -21,5 +21,25 @@ class ArrayOfAnything<T> {
 
 // TEST
 const test = new ArrayOfAnything<number>([1, 2]);
-console.log(test.get(1))
+console.log(test.get(1));
 
+// Generics with functions
+function printStrings(arr: string[]): void {
+   for (let index = 0; index < arr.length; index++) {
+      console.log(arr[index]);
+   }
+}
+
+function printNumbers(arr: number[]): void {
+   for (let index = 0; index < arr.length; index++) {
+      console.log(arr[index]);
+   }
+}
+
+function printAnything<T>(arr: T[]) {
+   for (let index = 0; index < arr.length; index++) {
+      console.log(arr[index]);
+   }
+}
+
+printAnything<string>(['a', 'b', 'c']);
