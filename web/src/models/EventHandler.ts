c532981977adb122bb35a276bd1ad8a534863a53
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-12-29 09:06:20 +0700

    fetch data

--- web/src/models/EventHandler.ts
+++ web/src/models/EventHandler.ts
@@ -1,25 +1,23 @@
-
-
 type Callback = () => void;
 
 export class EventHandler {
    events: { [key: string]: Callback[] } = {};
 
-   on(eventName: string, callback: Callback) {
+   on = (eventName: string, callback: Callback) => {
       const handlers = this.events[eventName] || [];
       handlers.push(callback);
       this.events[eventName] = handlers;
-   }
+   };
 
-   trigger(eventName: string): void {
+   trigger = (eventName: string): void => {
       const handlers = this.events[eventName];
+
       if (!handlers || handlers.length === 0) {
          return;
       }
+
       handlers.forEach((callback) => {
          callback();
       });
-   }
-
-   
+   };
 }
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,6 +1,7 @@
 import { EventHandler } from './EventHandler';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios';
 
 const serverUrl = 'http://localhost:3000/users';
 export interface UserProps {
@@ -10,11 +11,38 @@ export interface UserProps {
 }
 
 export class User {
-   public event = new EventHandler();
+   public events = new EventHandler();
    public sync: Sync<UserProps> = new Sync<UserProps>(serverUrl);
    public attributes: Attributes<UserProps>;
 
    constructor(attrs: UserProps) {
       this.attributes = new Attributes<UserProps>(attrs);
+   }
+
+   get on() {
+      return this.events.on;
+   }
+
+   get trigger() {
+      return this.events.trigger;
+   }
+
+   get get() {
+      return this.attributes.get;
+   }
+
+   set(update: UserProps): void {
+      this.attributes.set(update);
+      this.events.trigger('change');
+   }
+
+   fetch(): void {
+      const id = this.attributes.get('id');
+      if (typeof id !== 'number') {
+         throw new Error('Can not fetch without an id');
+      }
+      this.sync.fetch(id).then((response: AxiosResponse): void => {
+         this.set(response.data);
+      });
    }
 }
